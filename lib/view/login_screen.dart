c61be9ca002660524c6f573a62aa2a13ca5916e6
chandan kumar <kumarchandan41u@gmail.com>
2022-06-17 23:09:15 +0530

    loginscreen

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mvvm/resource/components/roundButton.dart';
 import 'package:mvvm/utils/utils.dart';
 
 class LoginView extends StatefulWidget {
@@ -9,13 +11,86 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  ValueNotifier<bool> _obsecuredPassword = ValueNotifier<bool>(true);
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(
-      body: Column(
-        children: [
-          TextFormField()
-        ],
+      appBar: AppBar(
+        title: const Text("Login"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              focusNode: emailFocusNode,
+              decoration: const InputDecoration(
+                hintText: "Email",
+                labelText: 'Email',
+                prefixIcon: Icon(Icons.alternate_email),
+              ),
+              onFieldSubmitted: (_) {
+                Utils.filedFocusChange(
+                    context, emailFocusNode, passwordFocusNode);
+              },
+            ),
+            ValueListenableBuilder(
+              valueListenable: _obsecuredPassword,
+              builder: (context, value, child) {
+                return TextFormField(
+                  controller: _passwordController,
+                  focusNode: passwordFocusNode,
+                  obscureText: _obsecuredPassword.value,
+                  obscuringCharacter: "*",
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock_open_outlined),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        _obsecuredPassword.value = !_obsecuredPassword.value;
+                      },
+                      child: Icon(
+                        _obsecuredPassword.value
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: height * .1,
+            ),
+            RoundButton(
+              title: 'Login',
+              onPress: () {
+                if (_emailController.text.isEmpty) {
+                  Utils.flushBarErrorMessage('Please enter email', context);
+                } else if (_passwordController.text.isEmpty) {
+                  Utils.flushBarErrorMessage('Please enter password', context);
+                } else if (_passwordController.text.length < 6) {
+                  Utils.flushBarErrorMessage(
+                      'Please enter 6 digit password', context);
+                } else {
+                  print("api hit");
+                }
+              },
+              // loading: true,
+            ),
+          ],
+        ),
       ),
     );
   }
